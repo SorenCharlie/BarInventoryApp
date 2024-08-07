c7da68882958c0636d8a4784775668cc74888e8d
Wolfgang Leithold <wolfleithold@gmail.com>
2024-08-06 23:21:11 -0700

    fixed error in seed data

--- seeds/drinkIngredients.js
+++ seeds/drinkIngredients.js
@@ -1,64 +1,64 @@
-const { DrinkIngredient } = require('../models');
+const { DrinkIngredient } = require("../models");
 
 const drinkIngredientData = [
   {
-    drinkId: 1, // Jack and Coke  
-    ingredientId: 1, // Jack Daniels  
+    drinkId: 1, // Jack and Coke
+    ingredientId: 1, // Jack Daniels
     quantity: 1,
   },
   {
-    drinkId: 1, // Jack and Coke  
-    ingredientId: 2, // Coca-Cola  
+    drinkId: 1, // Jack and Coke
+    ingredientId: 2, // Coca-Cola
     quantity: 1,
   },
   {
-    drinkId: 2, // Margarita  
-    ingredientId: 3, // Tequila  
+    drinkId: 2, // Margarita
+    ingredientId: 3, // Tequila
     quantity: 1,
   },
   {
-    drinkId: 2, // Margarita  
-    ingredientId: 4, // Triple Sec  
+    drinkId: 2, // Margarita
+    ingredientId: 4, // Triple Sec
     quantity: 1,
   },
   {
-    drinkId: 2, // Margarita  
-    ingredientId: 5, // Lime Juice  
+    drinkId: 2, // Margarita
+    ingredientId: 5, // Lime Juice
     quantity: 1,
   },
   {
-    drinkId: 2, // Margarita  
-    ingredientId: 6, // Salt  
+    drinkId: 2, // Margarita
+    ingredientId: 6, // Salt
     quantity: 1,
   },
   {
-    drinkId: 3, // Mojito  
-    ingredientId: 7, // Mint Leaves  
+    drinkId: 3, // Mojito
+    ingredientId: 7, // Mint Leaves
     quantity: 1,
   },
   {
-    drinkId: 3, // Mojito  
+    drinkId: 3, // Mojito
     ingredientId: 8, // White Rum
     quantity: 1,
   },
   {
-    drinkId: 3, // Mojito 
-    ingredientId: 9, // Sugar Syrup 
+    drinkId: 3, // Mojito
+    ingredientId: 9, // Sugar Syrup
     quantity: 1,
   },
   {
-    drinkId: 3, // Mojito  
-    ingredientId: 5, // Lime Juice  
-    quantity: 1,
-  },
-  {
-    drinkId: 4, // Gin and Tonic
-    ingredientId: 10 // Gin
+    drinkId: 3, // Mojito
+    ingredientId: 5, // Lime Juice
     quantity: 1,
   },
   {
     drinkId: 4, // Gin and Tonic
-    ingredientId: 11 // Tonic
+    ingredientId: 10, // Gin
+    quantity: 1,
+  },
+  {
+    drinkId: 4, // Gin and Tonic
+    ingredientId: 11, // Tonic
     quantity: 1,
   },
   {
@@ -68,12 +68,12 @@ const drinkIngredientData = [
   },
   {
     drink_id: 5, // Rum and Coke
-    ingredientId: 2 // Coka-Cola
+    ingredientId: 2, // Coka-Cola
     quantity: 1,
   },
-
 ];
 
-const seedDrinkIngredients = () => DrinkIngredient.bulkCreate(drinkIngredientData);
+const seedDrinkIngredients = () =>
+  DrinkIngredient.bulkCreate(drinkIngredientData);
 
 module.exports = seedDrinkIngredients;
