36028f9f48f4bf2f9e80457afaf534482a30f4f6
Wolfgang Leithold <wolfleithold@gmail.com>
2024-08-10 18:03:19 -0700

    fixed changes

--- db/users.sql
+++ db/users.sql
@@ -1,7 +1,5 @@
--- Drop the table if it exists (to avoid conflicts)
 DROP TABLE IF EXISTS users;
 
--- Create the table
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username VARCHAR(50) UNIQUE NOT NULL,
