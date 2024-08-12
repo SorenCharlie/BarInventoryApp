d9507054e3cf7cd8fe992811121d344197ab2592
KobESB <robertskobie@gmail.com>
2024-08-11 18:34:35 -0700

    cards

---
+++ Cards-1.js
@@ -0,0 +1,17 @@
+
+document.getElementById('replenishButton').addEventListener('click',function(event){
+    event.preventDerfault();
+    let currentQuantity = document.getElementById() =parseInt(document.getElementById('quantity').text.Content);
+    let replenishQuantity = currentQuantity + 5000;
+
+// Update the quantity
+    document.getElementById('quantity').textContent = replenishQuantity;
+
+    alert('Replenished Successfully' + replenishQuantity);
+
+
+
+
+
+
+});
