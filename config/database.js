429dab543a75700a41439f64f5f422298d568d3f
Wolfgang Leithold <wolfleithold@gmail.com>
2024-08-13 19:21:55 -0700

    changed database

--- config/database.js
+++ config/database.js
@@ -1,10 +1,20 @@
-const { Sequelize } = require("sequelize");
+const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize("barinventory_db", "postgres", "password", {
-  host: process.env.DB_HOST,
-  dialect: "postgres",
-  logging: console.log,
-});
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "postgres",
+    }
+  );
+}
 
 module.exports = sequelize;
